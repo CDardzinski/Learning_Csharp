cd9a8f25eed2004b0bc430159ff274fac54e72e5
CDardzinski <126962333+CDardzinski@users.noreply.github.com>
2023-04-09 18:42:48 +0200

    Add files via upload

---
+++ Owoce_Csharp_roz2_przyklad2_5/Owoce_Csharp_roz2_przy2_5/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Owoce_Csharp_roz2_przy2_5
+{
+    partial class cardFlip
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardBackPictureBox = new System.Windows.Forms.PictureBox();
+            this.cardFacePictureBox = new System.Windows.Forms.PictureBox();
+            this.showBackButton = new System.Windows.Forms.Button();
+            this.showFaceButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBackPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFacePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardBackPictureBox
+            // 
+            this.cardBackPictureBox.Image = global::Owoce_Csharp_roz2_przy2_5.Properties.Resources.Backface_Blue;
+            this.cardBackPictureBox.Location = new System.Drawing.Point(27, 22);
+            this.cardBackPictureBox.Name = "cardBackPictureBox";
+            this.cardBackPictureBox.Size = new System.Drawing.Size(294, 323);
+            this.cardBackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardBackPictureBox.TabIndex = 0;
+            this.cardBackPictureBox.TabStop = false;
+            // 
+            // cardFacePictureBox
+            // 
+            this.cardFacePictureBox.Image = global::Owoce_Csharp_roz2_przy2_5.Properties.Resources.Ace_Spades;
+            this.cardFacePictureBox.Location = new System.Drawing.Point(450, 24);
+            this.cardFacePictureBox.Name = "cardFacePictureBox";
+            this.cardFacePictureBox.Size = new System.Drawing.Size(278, 321);
+            this.cardFacePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardFacePictureBox.TabIndex = 1;
+            this.cardFacePictureBox.TabStop = false;
+            this.cardFacePictureBox.Visible = false;
+            // 
+            // showBackButton
+            // 
+            this.showBackButton.Location = new System.Drawing.Point(120, 377);
+            this.showBackButton.Name = "showBackButton";
+            this.showBackButton.Size = new System.Drawing.Size(75, 39);
+            this.showBackButton.TabIndex = 2;
+            this.showBackButton.Text = "Pokaż tył karty";
+            this.showBackButton.UseVisualStyleBackColor = true;
+            this.showBackButton.Click += new System.EventHandler(this.showBackButton_Click);
+            // 
+            // showFaceButton
+            // 
+            this.showFaceButton.Location = new System.Drawing.Point(547, 377);
+            this.showFaceButton.Name = "showFaceButton";
+            this.showFaceButton.Size = new System.Drawing.Size(75, 39);
+            this.showFaceButton.TabIndex = 3;
+            this.showFaceButton.Text = "Pokaż przód karty";
+            this.showFaceButton.UseVisualStyleBackColor = true;
+            this.showFaceButton.Click += new System.EventHandler(this.showFaceButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(338, 415);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Zakończ";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // cardFlip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.showFaceButton);
+            this.Controls.Add(this.showBackButton);
+            this.Controls.Add(this.cardFacePictureBox);
+            this.Controls.Add(this.cardBackPictureBox);
+            this.Name = "cardFlip";
+            this.Text = "Card Filp";
+            ((System.ComponentModel.ISupportInitialize)(this.cardBackPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFacePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardBackPictureBox;
+        private System.Windows.Forms.PictureBox cardFacePictureBox;
+        private System.Windows.Forms.Button showBackButton;
+        private System.Windows.Forms.Button showFaceButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
