5ca7626bb3051092d31daa59c624740a796f520a
CDardzinski <126962333+CDardzinski@users.noreply.github.com>
2023-04-09 18:42:03 +0200

    Add files via upload

---
+++ Owoce_Csharp_roz2_przyklad2_4/Owoce_Csharp_roz2_przyklad2_4/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Owoce_Csharp_roz2_przyklad2_4
+{
+    public partial class Flags : Form
+    {
+        public Flags()
+        {
+            InitializeComponent();
+        }
+
+        private void finlandPictureBox_Click(object sender, EventArgs e)
+        {
+            countryLabel.Text = "Finlandia";
+        }
+
+        private void francePictureBox_Click(object sender, EventArgs e)
+        {
+            countryLabel.Text = "Francja";
+        }
+
+        private void germanyPictureBox_Click(object sender, EventArgs e)
+        {
+            countryLabel.Text = "Niemcy";
+        }
+    }
+}
