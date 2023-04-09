6fc12aff20d151696e715a94b5936b41c44357a3
CDardzinski <126962333+CDardzinski@users.noreply.github.com>
2023-04-09 18:45:22 +0200

    Add files via upload

---
+++ Owoce_Csharp_roz2_cw2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Owoce_Csharp_roz2_cw2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.OnePictureBox = new System.Windows.Forms.PictureBox();
+            this.TwoPictureBox = new System.Windows.Forms.PictureBox();
+            this.ThreePictureBox = new System.Windows.Forms.PictureBox();
+            this.FourPictureBox = new System.Windows.Forms.PictureBox();
+            this.FivePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.OnePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TwoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ThreePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FourPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FivePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(330, 268);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(75, 23);
+            this.QuitButton.TabIndex = 0;
+            this.QuitButton.Text = "Wyjście";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // OnePictureBox
+            // 
+            this.OnePictureBox.Image = global::Owoce_Csharp_roz2_cw2.Properties.Resources.One;
+            this.OnePictureBox.Location = new System.Drawing.Point(40, 80);
+            this.OnePictureBox.Name = "OnePictureBox";
+            this.OnePictureBox.Size = new System.Drawing.Size(95, 163);
+            this.OnePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.OnePictureBox.TabIndex = 1;
+            this.OnePictureBox.TabStop = false;
+            this.OnePictureBox.Click += new System.EventHandler(this.OnePictureBox_Click);
+            // 
+            // TwoPictureBox
+            // 
+            this.TwoPictureBox.Image = global::Owoce_Csharp_roz2_cw2.Properties.Resources.Two;
+            this.TwoPictureBox.Location = new System.Drawing.Point(171, 80);
+            this.TwoPictureBox.Name = "TwoPictureBox";
+            this.TwoPictureBox.Size = new System.Drawing.Size(95, 163);
+            this.TwoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TwoPictureBox.TabIndex = 2;
+            this.TwoPictureBox.TabStop = false;
+            this.TwoPictureBox.Click += new System.EventHandler(this.TwoPictureBox_Click);
+            // 
+            // ThreePictureBox
+            // 
+            this.ThreePictureBox.Image = global::Owoce_Csharp_roz2_cw2.Properties.Resources.Three;
+            this.ThreePictureBox.Location = new System.Drawing.Point(320, 80);
+            this.ThreePictureBox.Name = "ThreePictureBox";
+            this.ThreePictureBox.Size = new System.Drawing.Size(95, 163);
+            this.ThreePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ThreePictureBox.TabIndex = 3;
+            this.ThreePictureBox.TabStop = false;
+            this.ThreePictureBox.Click += new System.EventHandler(this.ThreePictureBox_Click);
+            // 
+            // FourPictureBox
+            // 
+            this.FourPictureBox.Image = global::Owoce_Csharp_roz2_cw2.Properties.Resources.Four;
+            this.FourPictureBox.Location = new System.Drawing.Point(458, 80);
+            this.FourPictureBox.Name = "FourPictureBox";
+            this.FourPictureBox.Size = new System.Drawing.Size(95, 163);
+            this.FourPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FourPictureBox.TabIndex = 4;
+            this.FourPictureBox.TabStop = false;
+            this.FourPictureBox.Click += new System.EventHandler(this.FourPictureBox_Click);
+            // 
+            // FivePictureBox
+            // 
+            this.FivePictureBox.Image = global::Owoce_Csharp_roz2_cw2.Properties.Resources.Five;
+            this.FivePictureBox.Location = new System.Drawing.Point(604, 80);
+            this.FivePictureBox.Name = "FivePictureBox";
+            this.FivePictureBox.Size = new System.Drawing.Size(95, 163);
+            this.FivePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FivePictureBox.TabIndex = 5;
+            this.FivePictureBox.TabStop = false;
+            this.FivePictureBox.Click += new System.EventHandler(this.FivePictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 342);
+            this.Controls.Add(this.FivePictureBox);
+            this.Controls.Add(this.FourPictureBox);
+            this.Controls.Add(this.ThreePictureBox);
+            this.Controls.Add(this.TwoPictureBox);
+            this.Controls.Add(this.OnePictureBox);
+            this.Controls.Add(this.QuitButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.OnePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TwoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ThreePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FourPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FivePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.PictureBox OnePictureBox;
+        private System.Windows.Forms.PictureBox TwoPictureBox;
+        private System.Windows.Forms.PictureBox ThreePictureBox;
+        private System.Windows.Forms.PictureBox FourPictureBox;
+        private System.Windows.Forms.PictureBox FivePictureBox;
+    }
+}
+
---
+++ Owoce_Csharp_roz2_cw2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Owoce_Csharp_roz2_cw2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void OnePictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jeden");
+        }
+        private void TwoPictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dwa");
+        }
+
+        private void ThreePictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Trzy");
+        }
+
+        private void FourPictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cztery");
+        }
+        private void FivePictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pięć");
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
