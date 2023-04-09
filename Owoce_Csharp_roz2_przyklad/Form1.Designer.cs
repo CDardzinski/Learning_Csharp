0a0b51a0fdb0be8d7260ae003dd680db7242a2e6
CDardzinski <126962333+CDardzinski@users.noreply.github.com>
2023-04-09 18:36:11 +0200

    Add files via upload

---
+++ Owoce_Csharp_roz2_przyklad/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Owoce_Csharp_roz2_przyklad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateTaxButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calculateTaxButton
+            // 
+            this.calculateTaxButton.Location = new System.Drawing.Point(149, 257);
+            this.calculateTaxButton.Name = "calculateTaxButton";
+            this.calculateTaxButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateTaxButton.TabIndex = 0;
+            this.calculateTaxButton.Text = "Kliknij mnie";
+            this.calculateTaxButton.UseVisualStyleBackColor = true;
+            this.calculateTaxButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.calculateTaxButton);
+            this.Name = "Form1";
+            this.Text = "Mój pierwszy program";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateTaxButton;
+    }
+}
+
