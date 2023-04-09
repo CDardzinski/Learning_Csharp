0f7dabcdb0de41b607bca410e531313fc45b4db8
CDardzinski <126962333+CDardzinski@users.noreply.github.com>
2023-04-09 18:47:13 +0200

    Add files via upload

---
+++ Owoce_Csharp_roz2_cw3/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Owoce_Csharp_roz2_cw3
+{
+    partial class cardIDPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EightDiamondPictureBox = new System.Windows.Forms.PictureBox();
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.CardIdLabel = new System.Windows.Forms.Label();
+            this.TwoClubsPictureBox = new System.Windows.Forms.PictureBox();
+            this.KingSpadesPictureBox = new System.Windows.Forms.PictureBox();
+            this.AceSpadesPictureBox = new System.Windows.Forms.PictureBox();
+            this.RedJockerPictureBox = new System.Windows.Forms.PictureBox();
+            this.QuitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.EightDiamondPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TwoClubsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KingSpadesPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AceSpadesPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedJockerPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EightDiamondPictureBox
+            // 
+            this.EightDiamondPictureBox.Image = global::Owoce_Csharp_roz2_cw3.Properties.Resources._8_Diamonds;
+            this.EightDiamondPictureBox.Location = new System.Drawing.Point(12, 62);
+            this.EightDiamondPictureBox.Name = "EightDiamondPictureBox";
+            this.EightDiamondPictureBox.Size = new System.Drawing.Size(160, 232);
+            this.EightDiamondPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.EightDiamondPictureBox.TabIndex = 0;
+            this.EightDiamondPictureBox.TabStop = false;
+            this.EightDiamondPictureBox.Click += new System.EventHandler(this.EightDiamondPictureBox_Click);
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.AutoSize = true;
+            this.InfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.InfoLabel.Location = new System.Drawing.Point(229, 21);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(407, 25);
+            this.InfoLabel.TabIndex = 1;
+            this.InfoLabel.Text = "Kliknij kartę, aby wyświetlić jej nazwę";
+            // 
+            // CardIdLabel
+            // 
+            this.CardIdLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CardIdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CardIdLabel.Location = new System.Drawing.Point(123, 328);
+            this.CardIdLabel.Name = "CardIdLabel";
+            this.CardIdLabel.Size = new System.Drawing.Size(591, 37);
+            this.CardIdLabel.TabIndex = 2;
+            this.CardIdLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CardIdLabel.Click += new System.EventHandler(this.CardIdLabel_Click);
+            // 
+            // TwoClubsPictureBox
+            // 
+            this.TwoClubsPictureBox.Image = global::Owoce_Csharp_roz2_cw3.Properties.Resources._2_Clubs;
+            this.TwoClubsPictureBox.Location = new System.Drawing.Point(178, 62);
+            this.TwoClubsPictureBox.Name = "TwoClubsPictureBox";
+            this.TwoClubsPictureBox.Size = new System.Drawing.Size(160, 232);
+            this.TwoClubsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TwoClubsPictureBox.TabIndex = 3;
+            this.TwoClubsPictureBox.TabStop = false;
+            this.TwoClubsPictureBox.Click += new System.EventHandler(this.TwoClubsPictureBox_Click);
+            // 
+            // KingSpadesPictureBox
+            // 
+            this.KingSpadesPictureBox.Image = global::Owoce_Csharp_roz2_cw3.Properties.Resources.King_Spades;
+            this.KingSpadesPictureBox.Location = new System.Drawing.Point(344, 62);
+            this.KingSpadesPictureBox.Name = "KingSpadesPictureBox";
+            this.KingSpadesPictureBox.Size = new System.Drawing.Size(160, 232);
+            this.KingSpadesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.KingSpadesPictureBox.TabIndex = 4;
+            this.KingSpadesPictureBox.TabStop = false;
+            this.KingSpadesPictureBox.Click += new System.EventHandler(this.KingSpadesPictureBox_Click);
+            // 
+            // AceSpadesPictureBox
+            // 
+            this.AceSpadesPictureBox.Image = global::Owoce_Csharp_roz2_cw3.Properties.Resources.Ace_Spades;
+            this.AceSpadesPictureBox.Location = new System.Drawing.Point(510, 62);
+            this.AceSpadesPictureBox.Name = "AceSpadesPictureBox";
+            this.AceSpadesPictureBox.Size = new System.Drawing.Size(160, 232);
+            this.AceSpadesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.AceSpadesPictureBox.TabIndex = 5;
+            this.AceSpadesPictureBox.TabStop = false;
+            this.AceSpadesPictureBox.Click += new System.EventHandler(this.AceSpadesPictureBox_Click);
+            // 
+            // RedJockerPictureBox
+            // 
+            this.RedJockerPictureBox.Image = global::Owoce_Csharp_roz2_cw3.Properties.Resources.Joker_Red;
+            this.RedJockerPictureBox.Location = new System.Drawing.Point(676, 62);
+            this.RedJockerPictureBox.Name = "RedJockerPictureBox";
+            this.RedJockerPictureBox.Size = new System.Drawing.Size(160, 232);
+            this.RedJockerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.RedJockerPictureBox.TabIndex = 6;
+            this.RedJockerPictureBox.TabStop = false;
+            this.RedJockerPictureBox.Click += new System.EventHandler(this.RedJockerPictureBox_Click);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(380, 393);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(75, 23);
+            this.QuitButton.TabIndex = 7;
+            this.QuitButton.Text = "Wyjście";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // cardIDPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.RedJockerPictureBox);
+            this.Controls.Add(this.AceSpadesPictureBox);
+            this.Controls.Add(this.KingSpadesPictureBox);
+            this.Controls.Add(this.TwoClubsPictureBox);
+            this.Controls.Add(this.CardIdLabel);
+            this.Controls.Add(this.InfoLabel);
+            this.Controls.Add(this.EightDiamondPictureBox);
+            this.Name = "cardIDPanel";
+            this.Text = "Card Identifier";
+            ((System.ComponentModel.ISupportInitialize)(this.EightDiamondPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TwoClubsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KingSpadesPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AceSpadesPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedJockerPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox EightDiamondPictureBox;
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.Label CardIdLabel;
+        private System.Windows.Forms.PictureBox TwoClubsPictureBox;
+        private System.Windows.Forms.PictureBox KingSpadesPictureBox;
+        private System.Windows.Forms.PictureBox AceSpadesPictureBox;
+        private System.Windows.Forms.PictureBox RedJockerPictureBox;
+        private System.Windows.Forms.Button QuitButton;
+    }
+}
+
---
+++ Owoce_Csharp_roz2_cw3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Owoce_Csharp_roz2_cw3
+{
+    public partial class cardIDPanel : Form
+    {
+        public cardIDPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void CardIdLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EightDiamondPictureBox_Click(object sender, EventArgs e)
+        {
+            CardIdLabel.Text = "Ósemka Karo";
+        }
+
+        private void AceSpadesPictureBox_Click(object sender, EventArgs e)
+        {
+            CardIdLabel.Text = "As Pik";
+        }
+
+        private void KingSpadesPictureBox_Click(object sender, EventArgs e)
+        {
+            CardIdLabel.Text = "Król Pik";
+        }
+
+        private void TwoClubsPictureBox_Click(object sender, EventArgs e)
+        {
+            CardIdLabel.Text = "Dwójka trefl";
+        }
+
+        private void RedJockerPictureBox_Click(object sender, EventArgs e)
+        {
+            CardIdLabel.Text = "Czerwony Joker";
+        }
+    }
+}
